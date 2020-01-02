32b68a66ccf3d894287e47b753381b8c30482c70
李洪伟 <17862806857@163.com>
2020-01-02 11:41:20 +0800

    project init

---
+++ coupon-app/src/test/java/com/itzhongzi/couponapp/CouponAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itzhongzi.couponapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
